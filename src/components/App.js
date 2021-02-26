3da30549bf206ab9bb18e0245b5fb7941c0a8406
Abdullah Numan <anewman15@hotmail.com>
2021-02-26 18:58:56 +0600

    Move App to components and use es6

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const App = () => (
+  <div className="App">
+    Hello, Welcome to your favorite Bookstore
+    {/* <BooksList />
+    <BooksForm /> */}
+  </div>
+);
+
+export default App;
