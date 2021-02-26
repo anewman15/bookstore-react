3d37c2b8ec0b5551a0e6e7e45c9820c1d7d16ccb
Visvaldas <visva.rapalis@gmail.com>
2021-02-26 16:14:10 +0200

    Add book component

---
+++ src/components/Book.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const Book = props => {
+  const { id, title, category } = props;
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{title}</td>
+      <td>{category}</td>
+    </tr>
+  );
+};
+
+Book.propTypes = {
+  id: PropTypes.number,
+}.isRequired;
+export default Book;
