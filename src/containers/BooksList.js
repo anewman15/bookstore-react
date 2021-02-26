fbb751b7da829ea70eec5855dbe8b2fb3d10dc92
Visvaldas <visva.rapalis@gmail.com>
2021-02-26 16:29:15 +0200

    Add test books to BooksList

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,15 +1,18 @@
+import Book from '../components/Book';
+
 const BooksList = () => (
   <div>
     <h1>Books List</h1>
     <table>
-      <th>
-        <tr>
-          <td>Book ID</td>
-          <td>Title</td>
-          <td>Category</td>
-        </tr>
-      </th>
-      {/* <Book /> */}
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      <tbody>
+        <Book id={1} title="about memory" category="self improve" />
+        <Book id={2} title="dfdfdfd emory" category="ererererev improve" />
+      </tbody>
     </table>
   </div>
 );
