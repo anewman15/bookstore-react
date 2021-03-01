7fa36c361b1559530d40197a90b61e7ad42f3041
Visvaldas <visva.rapalis@gmail.com>
2021-03-01 10:03:27 +0200

    Define delete action for the book

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const DELETE_BOOK = 'DELETE_BOOK';
 
-const createBook = (book) => ({
+const createBook = book => ({
   type: CREATE_BOOK,
   book,
 });
 
-export createBook;
+const deleteBook = book => ({
+  type: DELETE_BOOK,
+  book,
+});
+
+export { createBook, deleteBook };
