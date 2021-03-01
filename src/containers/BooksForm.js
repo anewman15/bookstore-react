fd256a714ebdb5c3880f0e565200b633b4294e05
Visvaldas <visva.rapalis@gmail.com>
2021-03-01 15:55:23 +0200

    Import useState and create state for book

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const BooksForm = props => {
   const { categories } = props;
+  const [book, setBook ] = useState({});
 
   return (
     <form>
