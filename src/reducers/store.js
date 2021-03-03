95b0c02f1db680eb8fe0e119e80fbc3ebd41512c
Abdullah Numan <anewman15@hotmail.com>
2021-03-03 20:13:57 +0600

    Pass argument for initial state in createStore function

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,6 +1,19 @@
 import { createStore } from 'redux';
-import rootReducer from './index';
+import combinedReducer from './index';
 
-const store = createStore(rootReducer);
+const initialBookState = [
+  {
+    id: Math.floor(Math.random() * 10000000),
+    title: 'A Brief History of Time',
+    category: 'Sci-Fi',
+  },
+  {
+    id: Math.floor(Math.random() * 10000000),
+    title: "Harry Potter: Sorcerer's Store",
+    category: 'Kids',
+  },
+];
+
+const store = createStore(combinedReducer, { books: initialBookState, filter: 'All' });
 
 export default store;
