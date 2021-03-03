86828fa3918586326a5ffb201fb6cc25cb30ec4d
Abdullah Numan <anewman15@hotmail.com>
2021-03-03 20:12:02 +0600

    Remove initialBookState, change bookReducer function name, fix iterable error

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,45 +1,28 @@
-const initialBookState = {
-  books: [
-    {
-      id: Math.floor(Math.random() * 10000000),
-      title: 'A Brief History of Time',
-      category: 'Sci-Fi',
-    },
-    {
-      id: Math.floor(Math.random() * 10000000),
-      title: "Harry Potter: Sorcerer's Store",
-      category: 'Kids',
-    },
-  ],
-};
-
-const bookReducer = (state = initialBookState, action) => {
+const books = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_BOOK': {
       const { id, title, category } = action.payload;
-      return {
-        ...state,
-        books: [
-          ...state.books,
+      return (
+        [
+          ...state,
           {
             id,
             title,
             category,
           },
-        ],
-      };
+        ]
+      );
     }
     case 'DELETE_BOOK': {
       const { id } = action.payload;
-      return {
-        ...state,
-        books: [...state.books].filter(book => book.id !== id),
-      };
+      return (
+        [...state].filter(book => book.id !== id)
+      );
     }
     default: {
-      return state.books;
+      return state;
     }
   }
 };
 
-export default bookReducer;
+export default books;
