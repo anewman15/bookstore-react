91c0b3e832a2ad8ea0f1f8300d854d8d990cd091
Abdullah Numan <anewman15@hotmail.com>
2021-03-03 16:02:27 +0600

    Create handleFilterChange func and call from select options

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,16 +1,27 @@
 import PropTypes from 'prop-types';
-import connect from 'react-redux';
+import { connect } from 'react-redux';
 import { changeFilter } from '../actions/index';
 
-const categoryFilter = () => {
+const categoryFilter = ({ changeFilter }) => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+  const handleFilterChange = event => {
+    changeFilter(event.target.value);
+  };
+
   return (
     <div>
       <span>Filter books by category</span>
       <select name="filter" id="filter">
-        <option value="all">All</option>
+        <option value="all" onChange={handleFilterChange}>All</option>
         {categories.map(category => (
-          <option key={category} value={category.toLowerCase()}>{category}</option>
+          <option
+            key={category}
+            value={category.toLowerCase()}
+            onChange={handleFilterChange}
+          >
+            {category}
+          </option>
         ))}
       </select>
     </div>
@@ -18,7 +29,7 @@ const categoryFilter = () => {
 };
 
 categoryFilter.propTypes = {
-  category: PropTypes.string,
+  category: PropTypes.instanceOf(Object),
 }.isRequired;
 
 export default connect(null, { changeFilter })(categoryFilter);
