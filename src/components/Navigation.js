806aa940a5ca8c1d9321272910fe7a270e95df0b
Visvaldas <visva.rapalis@gmail.com>
2021-03-04 14:41:03 +0200

    Create Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+const Navigation = () => (
+  <div className="container">
+    <nav className="navigation">
+      <div className="left-nav">
+        <h1 className="title">Little Princess BookStore</h1>
+        <h2 className="nav-item">Books</h2>
+        <h2 className="nav-item categories">Categories</h2>
+      </div>
+      <div className="right-nav">
+        <img src="user_icon.png" alt="user" className="user_icon" />
+      </div>
+    </nav>
+  </div>
+);
+
+export default Navigation;
