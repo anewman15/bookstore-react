64a4bca99a35ae8727c51aace508490579069309
Abdullah Numan <anewman15@hotmail.com>
2021-03-04 20:36:44 +0600

    Revamp Book comp and add styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,12 +8,41 @@ const Book = ({ book, deleteBook }) => {
   };
 
   return (
-    <tr>
-      <td>{book.id}</td>
-      <td>{book.title}</td>
-      <td>{book.category}</td>
-      <td><button type="button" onClick={handleRemoveBook}>Delete</button></td>
-    </tr>
+    <div className="book-wrapper flex-between">
+      <div className="book-left">
+        <div className="book-info">
+          <div className="book-detail">
+            <p className="montserrat font-size-14 color-grey-1 book-detail">{book.category}</p>
+            <p className="roboto font-size-22 text-bold book-detail">{book.title}</p>
+            <p className="roboto font-size-14 color-light-blue book-detail">Visvaldas Rapalis</p>
+          </div>
+          <div className="book-actions">
+            <button className="btn roboto font-size-14 color-light-blue" type="button">Comments</button>
+            <button className="btn btn-center roboto font-size-14 color-light-blue" type="button" onClick={handleRemoveBook}>Remove</button>
+            <button className="btn roboto font-size-14 color-light-blue" type="button">Edit</button>
+          </div>
+        </div>
+      </div>
+      <div className="book-right flex-between">
+        <div className="book-completed flex-between">
+          <div className="">
+            <img className="user-icon" src="user_icon.png" alt="percent-completed" />
+          </div>
+          <div>
+            <p>64%</p>
+            <p>Completed</p>
+          </div>
+        </div>
+        <div className="current-chapter">
+          <div>
+            <p>Current Chapter</p>
+            <p>Chapter 17</p>
+          </div>
+          <button type="button">Update Progress</button>
+        </div>
+
+      </div>
+    </div>
   );
 };
 
