eb3de9aadebdd72cbf625d12bae07e881751dabf
Abdullah Numan <anewman15@hotmail.com>
2021-03-04 20:36:19 +0600

    Add border to Navigation comp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,17 @@
 const Navigation = () => (
-  <div className="container">
-    <nav className="navigation">
-      <div className="left-nav">
-        <h1 className="title">Little Princess BookStore</h1>
-        <h2 className="nav-item">Books</h2>
-        <h2 className="nav-item categories">Categories</h2>
-      </div>
-      <div className="right-nav">
-        <img src="user_icon.png" alt="user" className="user_icon" />
-      </div>
-    </nav>
+  <div className="bg-white border-bottom">
+    <div className="container">
+      <nav className="navigation flex-between">
+        <div className="left-nav">
+          <h1 className="title">Little Princess BookStore</h1>
+          <h2 className="nav-item">Books</h2>
+          <h2 className="nav-item categories">Categories</h2>
+        </div>
+        <div className="right-nav">
+          <img src="user_icon.png" alt="user" className="user-icon" />
+        </div>
+      </nav>
+    </div>
   </div>
 );
 
