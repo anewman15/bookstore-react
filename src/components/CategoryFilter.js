ed3afc11402d94a3f1d8476ed9b206398265de8a
Abdullah Numan <anewman15@hotmail.com>
2021-03-04 14:07:52 +0600

    Adjust handleFilterChange to fix error

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,26 +1,21 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CategoryFilter = ({ changeFilter }) => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  const [categoryFilter, setcategoryFilter] = useState('All');
 
   const handleFilterChange = event => {
-    setcategoryFilter({
-      categoryFilter: event.target.value,
-    });
-    changeFilter(categoryFilter);
+    changeFilter(event.target.value);
   };
 
   return (
     <div>
       <span>Filter books by category</span>
-      <select value={categoryFilter} onChange={handleFilterChange}>
-        <option value="all">All</option>
+      <select onChange={handleFilterChange}>
+        <option value="All">All</option>
         {categories.map(category => (
           <option
             key={category}
-            value={category.toLowerCase()}
+            value={category}
             onChange={handleFilterChange}
           >
             {category}
@@ -35,4 +30,4 @@ CategoryFilter.propTypes = {
   category: PropTypes.instanceOf(Object),
 }.isRequired;
 
-export default (CategoryFilter);
+export default CategoryFilter;
